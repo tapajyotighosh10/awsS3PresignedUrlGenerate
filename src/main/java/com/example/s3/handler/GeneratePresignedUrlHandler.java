9074b6d13be3ccdbab9b34d9a2c1d2733eafaa89
tapajyotighosh10 <tapajyotighosh10@gmail.com>
2024-11-14 13:18:37 +0530

    added validation checks

--- src/main/java/com/example/s3/handler/GeneratePresignedUrlHandler.java
+++ src/main/java/com/example/s3/handler/GeneratePresignedUrlHandler.java
@@ -1,13 +1,14 @@
 package com.example.s3.handler;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.example.s3.model.FileRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GeneratePresignedUrlHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
@@ -22,23 +23,48 @@ public class GeneratePresignedUrlHandler implements RequestHandler<APIGatewayPro
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
 
+        if (BUCKET_NAME == null || BUCKET_NAME.isEmpty()) {
+            return response
+                    .withStatusCode(500)
+                    .withBody("{\"error\": \"Bucket name not set on environment variable\"}");
+        }
+
+        if (input == null || input.getQueryStringParameters() == null) {
+            return response
+                    .withStatusCode(400)
+                    .withBody("{\"error\": \"Request is missing\"}");
+        }
+
+        FileRequest fileRequest = FileRequest.from(input);
+        String filename = fileRequest.getFilename();
+
+        if (filename == null || filename.trim().isEmpty()) {
+            return response
+                    .withStatusCode(400)
+                    .withBody("{\"error\": \"Invalid input: filename required\"}");
+        }
         try {
 
-            FileRequest fileRequest = FileRequest.from(input);
-
-
-            String presignedUrl = GeneratePresignedUrl.generatePresignedUrl(BUCKET_NAME, fileRequest.getFilename());
-
-
-            String output = String.format("{ \"upload_url\": \"%s\" }", presignedUrl);
+            String presignedUrl = GeneratePresignedUrl.generatePresignedUrl(BUCKET_NAME, filename);
+            String output = String.format("{\"upload_url\": \"%s\"}", presignedUrl);
 
             return response
                     .withStatusCode(200)
                     .withBody(output);
-        } catch (NullPointerException e) {
+
+        } catch (S3Exception e) {
+            if (e.statusCode() == 403) {
+                return response
+                        .withStatusCode(403)
+                        .withBody("{\"error\": \"Set lambda IAM role permissions to generate presigned URL\"}");
+            }
             return response
-                    .withBody("{}")
-                    .withStatusCode(500);
+                    .withStatusCode(500)
+                    .withBody("{\"error\": \"Failed to generate presigned URL due to S3 error.\"}");
+        } catch (Exception e) {
+            return response
+                    .withStatusCode(500)
+                    .withBody("{\"error\": \"Failed to generate presigned URL\"}");
         }
     }
 }
